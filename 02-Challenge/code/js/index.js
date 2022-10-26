97cb90672909d71ee7133c859ea1a8aab7ac46f9
hannahk <hanhannah839@gmail.com>
2022-10-26 01:08:10 -0700

    adding CSS

--- 02-Challenge/code/js/index.js
+++ 02-Challenge/code/js/index.js
@@ -5,15 +5,15 @@ var questions = [
     title: 'How many sides are in a triangle?',
     answers: [
         {
-            answer: 1,
+            answer: '1 side',
             correct: false
         },
         {
-            answer: 2,
+            answer: '2 sides',
             correct: false
         },
         {
-            answer: 3,
+            answer: '3 sides',
             correct: true
         },
     ]
@@ -22,15 +22,15 @@ var questions = [
     title: 'How many feet does a penquin have?',
     answers: [
         {
-            answer: 1,
+            answer: '1 foot',
             correct: false
         },
         {
-            answer: 2,
+            answer: '2 feet',
             correct: true
         },
         {
-            answer: 33,
+            answer: '33 feet',
             correct: false
         },
     ]
@@ -57,17 +57,16 @@ var questions = [
 var currentQuestion = 0
         
 function homePage () {
- quiz.innerHTML = /*html*/`
+ quiz.innerHTML = `
     <h1>
         My Quiz
     </h1>
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id ipsum vitae risus tincidunt scelerisque vitae eget turpis. Pellentesque mauris nisl, lacinia id euismod at, auctor in urna. Nulla facilisi. Sed fringilla ultrices feugiat. Mauris vel aliquet nulla, at feugiat lorem. Vestibulum non elit consectetur, lacinia nulla eget, euismod lectus. Pellentesque consequat quam sit amet odio laoreet, sit amet tristique erat euismod. Vivamus sagittis ante a neque posuere, at euismod justo ultrices. Integer lacinia in enim sit amet ultricies. Nullam laoreet convallis turpis, vel hendrerit est facilisis sit amet.
     </p>
-    <button id="startQuiz">start quiz</button>
-    `
+    <button id="startQuiz">start quiz</button> `
 
-    document
+ document
     .getElementById('startQuiz')
     .addEventListener(
         'click',
@@ -76,7 +75,8 @@ function homePage () {
         }
     )
 
-}
+};
+
 homePage();
 
 var timerEl = document.getElementById('countdown');
@@ -98,17 +98,32 @@ function countdown() {
             gameOverScreen();
         }
     }, 1000);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
 
 countdown();
 
 function questionPage (quesEl) {
- quiz.innerHTML = /*html*/`
+ quiz.innerHTML = `
     <p>
     ${quesEl.title}
     </p>
     
-    <ul class="answerChoice" >
+    <div class="answerChoice" >
         <p>
             <button id="answerOne" data-correct="${quesEl.answers[0].correct}">${quesEl.answers[0].answer}</button>
         </p>
@@ -118,7 +133,7 @@ function questionPage (quesEl) {
         <p>
             <button id="answerThree" data-correct="${quesEl.answers[2].correct}">${quesEl.answers[2].answer}</button>
         </p>
-    </ul>
+    </div>
     `
 
  document
@@ -181,19 +196,21 @@ function questionPage (quesEl) {
      currentQuestion++;
          questionPage(questions[currentQuestion])
         })
-     
-}
+};
 
 questionPage();
 
 function gameOverScreen() {
-    quiz.innerHTML = /*html*/`
-        <p>Game Over</p>
+    quiz.innerHTML = `
+        <p id=gameOverTxt>GAME OVER</p>
         <p>You got a high score! Enter name:</p>
         <form><input type="text" class="form-input" placeholder="Your Name "></form>
+        <div class="buttons">
         <button id="submit-btn">Submit</button>
-        <button id="restart-btn">Restart</button>
+        <button id="restart-btn">Restart</button> 
+        </div>
     `
+
     document
     .querySelector('#submit-btn')
     .addEventListener(
@@ -210,34 +227,34 @@ function gameOverScreen() {
         function(GameOver) {
             window.location.reload();
         }
-    ) 
-     
-}
+    )  
+};
 
 gameOverScreen();
 
 function viewHighScore() {
    
-    var backEl  = document.getElementById("#goBack");
-    var resetEl = document.getElementById("#reset");
+    var backEl  = document.getElementById("#reset-game");
+    var resetEl = document.getElementById("#resetscore");
     
-    document
-        .querySelector(backEl)
-        .addEventListener(
-            'click',
-            function() {
-                history.back();
-            }
-        )
-    document
-        .querySelector(resetEl)
-        .addEventListener(
-            'click',
-            function() {
-                localStorage.clear();
-            }
-        )
-   
-}
+    backEl.addEventListener(
+        'click',
+        function() {
+           location.reload();
+        }
+    ) 
+
+    console.log('reset button clicked')
+
+    resetEl.addEventListener(
+        'click',
+        function() {
+            localStorage.clear();
+        }
+    )
+
+    console.log('cleared local storage')
+
+};
 
 viewHighScore();
